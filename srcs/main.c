5e2f56139ccf6753af8599aa42e9ddbe935e3cc7
Red alyn of the rosewood Rhaenys targaryen <rrhaenys@e2r3p1.21-school.ru>
2019-02-07 21:40:40 +0300

    There is no way

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 11:11:22 by jcorwin           #+#    #+#             */
-/*   Updated: 2019/02/07 21:33:20 by rrhaenys         ###   ########.fr       */
+/*   Updated: 2019/02/07 21:39:53 by rrhaenys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,21 +68,10 @@ int		ft_muve_turn(t_turn *turn)
 	return (step);
 }
 
-int		main()
+void	ft_free_turn(t_turn *turn)
 {
-	t_room	*start;
-	int		ants;
-	t_turn	*turn;
 	t_turn	*ptr;
 
-	ants = 3;
-	start =	ft_init_rooms(ants);
-	ft_init_len(start);
-	ft_sotr_len(start);
-//	ft_print_rooms(start);
-	turn = ft_init_turn_ant(ants, start);
-	while (ft_muve_turn(turn) > 0)
-		ft_printf("\n");
 	while (turn != NULL)
 	{
 		ptr = turn;
@@ -91,6 +80,39 @@ int		main()
 		free(ptr->name);
 		free(ptr);
 	}
+}
+
+int		ft_corr_rooms(t_room *start)
+{
+	int	index;
+
+	index = -1;
+	while (++index < start->link_size)
+		if (start->steps[index] >= 0)
+			return (1);
+	return (0);
+}
+
+int		main()
+{
+	t_room	*start;
+	int		ants;
+	t_turn	*turn;
+
+	ants = 3;
+	start =	ft_init_rooms(ants);
+	ft_init_len(start);
+	if (ft_corr_rooms(start) == 0)
+	{
+		ft_printf("There is no way!\n");
+		exit(0);
+	}
+	ft_sotr_len(start);
+//	ft_print_rooms(start);
+	turn = ft_init_turn_ant(ants, start);
+	while (ft_muve_turn(turn) > 0)
+		ft_printf("\n");
+	ft_free_turn(turn);
 //	ft_print_rooms(start);
 	room_del(start);
 	return (0);
